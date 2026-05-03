fd390fc4bc08e9732a9aa44bebb2723ba4c9a4af
tigerplush <christopher.putz@corgibase.de>
2026-05-03 13:21:56 +0200

    extract params and return type from given systems, check if return type is void

--- carrots/modules/carrots_ecs/include/system/into_system.hpp
+++ carrots/modules/carrots_ecs/include/system/into_system.hpp
@@ -1,6 +1,8 @@
 #ifndef CARROTS_ECS_SYSTEM_INTO_SYSTEM_HPP
 #define CARROTS_ECS_SYSTEM_INTO_SYSTEM_HPP
 
+#include <tuple>
+
 namespace carrots_ecs
 {
     namespace system
@@ -8,12 +10,28 @@ namespace carrots_ecs
 
         class IntoSystem
         {
+
         public:
             template <typename System>
             IntoSystem(System system)
             {
-                
+                using Traits = FunctionTraits<System>;
+                static_assert(std::is_same_v<Traits::ReturnType, void>, "System must return void");
             }
+
+        private:
+            template <typename T>
+            struct FunctionTraits
+            {
+                static_assert(false, "System must be a function");
+            };
+
+            template <typename Ret, typename... Args>
+            struct FunctionTraits<Ret (*)(Args...)>
+            {
+                using ReturnType = Ret;
+                using Arguments = std::tuple<Args...>;
+            };
         };
     } // namespace system
 
